d6cd118d7901aa34022409483d7038b9200e816a
santiago rodriguez matiz <s.rodriguezm@uniandes.edu.co>
2019-03-03 14:20:59 -0500

    REST-Producto-Promocion

--- app/app.js
+++ app/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tarjetaRegaloRouter = require('./routes/tarjetaRegalo');
+var promocionRouter = require('./routes/promocion');
+var productoRouter = require('./routes/producto');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tarjetaRegalo', tarjetaRegaloRouter);
+app.use('/promocion', promocionRouter);
+app.use('/producto', productoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
